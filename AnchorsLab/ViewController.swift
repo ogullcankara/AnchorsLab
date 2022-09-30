1dd93d410ecc659c9642b8179f1b1809e4142305
ogullcankara <ogullcankara@gmail.com>
2022-09-30 14:11:03 +0300

    First Commit

--- AnchorsLab/ViewController.swift
+++ AnchorsLab/ViewController.swift
@@ -11,9 +11,81 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        
     }
 
-
+    func setupViews() {
+        let upperLeftLabel = makeLabel(withText: "UpperLeft")
+        let upperRightLabel = makeLabel(withText: "UpperRight")
+        let lowerLeftLabel = makeSecondaryLabel(withText: "LowerLeft")
+        let lowerRigthButton = makeButton(withText: "lowerrigthButton")
+        let redView = makeView()
+        
+        view.addSubview(upperLeftLabel)
+        view.addSubview(upperRightLabel)
+        view.addSubview(lowerLeftLabel)
+        view.addSubview(lowerRigthButton)
+        view.addSubview(redView)
+        
+        upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        
+        upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        upperRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+        
+        lowerLeftLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 8).isActive = true
+        lowerLeftLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        
+        lowerRigthButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 8).isActive = true
+        lowerRigthButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+        
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        redView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        redView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        
+        
+    }
+    
+    func makeLabel(withText text:String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        
+        return label
+    }
+    
+    func makeSecondaryLabel(withText text:String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.backgroundColor = .yellow
+        label.textColor = .blue
+        
+        return label
+    }
+    
+    func makeButton(withText text:String) -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = .blue
+        
+        return button
+    }
+    
+    func makeView() -> UIView{
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = true
+        view.backgroundColor = .red
+        
+        return view
+    }
 }
 
